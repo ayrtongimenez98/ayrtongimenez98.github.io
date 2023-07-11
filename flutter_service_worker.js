524f5757273d25a22e83536c0dbe70df2affbd5f
Ayrton Gimenez <ayrtondavalos98@gmail.com>
2023-07-10 20:17:09 -0400

    Agregar nuevas funcionalidades

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/site.webmanifest": "29cb1727fc64fb6148018aa08144ad33",
-"index.html": "ef396c6208bb7831fffdc7932066a23c",
-"/": "ef396c6208bb7831fffdc7932066a23c",
-"main.dart.js": "46d96c15a882b7fca6f1ce46fe813880",
+"index.html": "48997f2a32bba0f3e53ee957d87718f3",
+"/": "48997f2a32bba0f3e53ee957d87718f3",
+"main.dart.js": "a94c1d3155948bc0e32296952a97efc1",
 "manifest.json": "7bc35365f80de35abc70ee96e5cb19d5",
 "version.json": "cc071df6862503f2c9acd042a012032d"
 };
